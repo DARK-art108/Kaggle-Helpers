edfddcade1ed001bd8ece304436dab6acf393b81
Ritesh Yadav <daydreamingguy941@gmail.com>
2022-08-10 17:50:14 +0530

    Create seed.py

---
+++ seed.py
@@ -0,0 +1,21 @@
+''' set_seed for Torch API '''
+def set_seed(seed = 42):
+    '''Sets the seed of the entire notebook so results are the same every time we run.
+    This is for REPRODUCIBILITY.'''
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    # When running on the CuDNN backend, two further options must be set
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    # Set a fixed value for the hash seed
+    os.environ['PYTHONHASHSEED'] = str(seed)
+
+''' set_seed for TF '''
+def seed_it_all(seed=7):
+    """ Attempt to be Reproducible """
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
